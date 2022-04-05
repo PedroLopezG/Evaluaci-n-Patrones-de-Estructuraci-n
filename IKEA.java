9f1a17e49d64248c729040157055fc40cad61f30
Pedro Manuel <98824525+PedroLopezG@users.noreply.github.com>
2022-04-05 19:17:16 +0200

    Add files via upload

---
+++ IKEA.java
@@ -0,0 +1,11 @@
+import Gestor.GestorVenta;
+
+public class IKEA {
+    public static void main(String[] args) {
+        GestorVenta venta = new GestorVenta();
+        venta.vender();
+
+
+
+    }
+}
