fd2f0cd1109f98a4e5c01a46f742e676b9074cd2
Pedro Manuel <98824525+PedroLopezG@users.noreply.github.com>
2022-04-05 19:20:51 +0200

    Add files via upload

---
+++ Almacen/Almacen.java
@@ -0,0 +1,11 @@
+package Almacen;
+import Modulos.Modulos;
+import Modulos.Kit;
+
+public class Almacen {
+    private Kit kits = new Kit();
+
+    public void comprobarStock(Modulos Modulo){
+
+    }
+}
